baa2190ee1a7cfe24707f4851696ce88d4319a21
WangXiaoDong <565374070@qq.com>
2017-11-28 16:02:39 +0800

    Initial commit

---
+++ src/main/java/com/wxd/App.java
@@ -0,0 +1,14 @@
+package com.wxd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
